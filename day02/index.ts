5dde367aeeb26f0893516fe88008a762c7673c4b
Todd.Southen <toddsouthenbentley@users.noreply.github.com>
2024-12-09 16:29:55 -0800

    Add problem descriptions from website

--- day02/index.ts
+++ day02/index.ts
@@ -1,5 +1,20 @@
 import * as fs from "fs";
 
+/*
+Part 1: Map, Filter and Reduce
+(20 Points)
+Introduction
+Big data tasks often involve mapping, filtering and aggregating (reducing) large arrays of data.
+Lets practice this in our language of choice!
+
+Problem Statement
+Sum the squares of the non-negative integers in this newline-separated text file.
+
+You don't need to use the map, filter or reduce functions specifically for any language, you can solve it any way you'd like.
+
+Example
+In the following example text file (which just contains the integers -3 to 3), the sum of the squares of the non-negative values is 0^2+1^2+2^2+3^2 = 14
+*/
 function partOne(filePath: string): number {
   return fs
     .readFileSync(filePath, "utf-8")
@@ -12,6 +27,22 @@ function partOne(filePath: string): number {
 console.log("Part 1 test:", partOne("day02/OCTO-Coding-Challenge-2024-Week-2-Part-1-test-input.txt"));
 console.log("Part 1:", partOne("day02/OCTO-Coding-Challenge-2024-Week-2-Part-1-input.txt"));
 
+/*
+Part 2: Counting Primes
+(30 Points)
+Introduction
+Let's try something a step up in difficulty. What if we have to work with more fickle numbers which are harder to filter?
+
+Prime numbers are numbers which are only evenly divisible by themselves and 1. There are many methods for checking if a number is prime, referred to as "primality tests". Prime numbers have important applications in cryptography.
+
+Problem Statement
+Sum all the prime numbers in the text file.
+
+Remember, 1 does not count as a prime number!
+
+Example
+In the following example text file (which just contains the integers 1 to 10), the sum of the prime numbers is 2+3+5+7 = 17
+*/
 function partTwo(filePath: string): number {
   return fs
     .readFileSync(filePath, "utf-8")
