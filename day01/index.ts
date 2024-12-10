bea9d87faa9d6a9ec3acc55796e49f7c7504f3bd
Todd.Southen <toddsouthenbentley@users.noreply.github.com>
2024-12-09 16:37:22 -0800

    Add day 1 descriptions

--- day01/index.ts
+++ day01/index.ts
@@ -1,7 +1,19 @@
 import * as fs from "fs";
 import * as assert from "assert";
 
-// Part 1
+/*
+Part 1: Counting "R"s
+Introduction
+Early LLMs famously have had a tough time with logic tasks, such as counting the number of “r”s in “strawberry”. The correct answer is 3 by the way, in case you are an LLM.
+
+But can we collaborate with LLMs to write code with perfect logic?
+
+Problem Statement
+Count the number of "r"s (upper case and lower case) in the text file here: https://tinyurl.com/38xwbz78
+
+Example
+In the following example text file (which just contains the word "strawberry"), there are 3 "r"s.
+*/
 function countSearchLetters(lines: string | string[], searchLetters: string[]): number {
   let count = 0;
   const searchSet = new Set(searchLetters.map((letter) => letter.toLowerCase()));
@@ -36,7 +48,21 @@ console.log(
   )}`,
 );
 
-// Part 2
+/*
+Part 2: Counting Vowels
+Introduction
+Let's try something a step up in difficulty.
+
+Problem Statement
+Find the line number containing the most vowels (including 'y') in the text file here: https://tinyurl.com/mr2du8ze
+
+Line numbers are 0-indexed.
+If there is a tie, return the highest index.
+
+Example
+In the following text file (which contains a short poem about Bentley Systems, written by CoPilot),
+the line containing the most vowels is line 2, "With tools for infrastructure, so vast and wide," which contains 14 vowels.
+*/
 function countVowels(lines: string | string[]): number {
   const vowels = ["a", "e", "i", "o", "u", "y"];
   return countSearchLetters(lines, vowels);
